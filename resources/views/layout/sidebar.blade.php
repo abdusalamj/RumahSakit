a49e318ca0170531ebff04afb27ffdc995ebd796
Abdusalam Junianto <juniantoabdusalam@gmail.com>
2018-10-09 14:50:29 +0700

    Template & penambahan routes

---
+++ resources/views/layout/sidebar.blade.php
@@ -0,0 +1,48 @@
+<div class="sidebar" data-color="purple" data-image="{{asset('template-admin/assets/img/sidebar-5.jpg')}}">
+    <div class="sidebar-wrapper">
+        <div class="logo">
+            <a class="simple-text">
+                Rumah Sakit
+            </a>
+        </div>
+
+        <ul class="nav">
+            <li class="active">
+                <a href="{{url('/kamar')}}">
+                    <i class="pe-7s-graph"></i>
+                    <p>Kamar</p>
+                </a>
+            </li>
+            <li>
+                <a href="{{url('/obat')}}">
+                    <i class="pe-7s-user"></i>
+                    <p>Obat</p>
+                </a>
+            </li>
+            <li>
+                <a href="{{url('/resep')}}">
+                    <i class="pe-7s-note2"></i>
+                    <p>Resep</p>
+                </a>
+            </li>
+            <li>
+                <a href="{{url('/pasien')}}">
+                    <i class="pe-7s-news-paper"></i>
+                    <p>Pasien</p>
+                </a>
+            </li>
+            <li>
+                <a href="{{url('/dokter')}}">
+                    <i class="pe-7s-science"></i>
+                    <p>Dokter</p>
+                </a>
+            </li>
+            <li>
+                <a href="{{url('/fasilitas')}}">
+                    <i class="pe-7s-map-marker"></i>
+                    <p>Fasilitas</p>
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
